12129665561a82017d0621d0d23e0753b830f987
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-11-15 12:19:09 -0800

    function to check whether user has moderator or admin role, if has neither then send message

--- middleware/authJwt.js
+++ middleware/authJwt.js
@@ -57,3 +57,25 @@ isModerator = (req, res, next) => {
         });
     });
 };
+
+isModeratorOdAdmin = (req, res, next) => {
+    User.findByPk(req.userId).then(user => {
+        user.getRoles().then(roles => {
+            for (let i = 0; i < roles.length; i++) {
+                if (roles[i].name === 'moderator') {
+                    next();
+                    return;
+                };
+
+                if (roles[i].name === 'admin') {
+                    next();
+                    return;
+                };
+            };
+
+            res.status(403).send({
+                message: 'Require Moderator or Admin Role!'
+            });
+        });
+    });
+};
