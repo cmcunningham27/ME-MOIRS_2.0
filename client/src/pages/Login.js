34d7ba80f3f9091a69f302efbff62fa204cdc8df
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-11-15 14:22:12 -0800

    started Login page with username and password in the state, returns a form that on submit calls a function that has not been declared yet, 2 input fields for the username and password, button, and a link to the signup form that hasnt been created yet if the user does not have an account yet

---
+++ client/src/pages/Login.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+
+export default function Login() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    return (
+        <div className='col-md-3 position-absolute top-50 start-50 translate-middle'>
+            <div className='card card-container p-4'>
+                <form onSubmit={loginUser}>
+                    <div className='form-group m-3'>
+                        <label className='mb-2' htmlFor='username'>Username</label>
+                        <input
+                            type='text'
+                            classNAme='form-control'
+                            name='username'
+                            value={username}
+                            minLength='3'
+                            required aria-required='true'
+                        />
+                    </div>
+
+                    <div className='form-group m-3'>
+                        <label className='mb-3' htmlFor='password'>Password</label>
+                        <input
+                            type='password'
+                            className='form-control'
+                            name='password'
+                            value={password}
+                            minLength='8'
+                            required aria-required='true'
+                        />
+                    </div>
+
+                    <button className='btn btn-primary btn-block'>Login</button>
+                    <p>
+                        Don't have an account? <Link to={'/signup'}>Signup</Link>
+                    </p>
+                </form>
+            </div>
+        </div>
+    )
+}
