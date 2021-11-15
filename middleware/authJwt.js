883f920ef3e5b3aeff38138e923b59fc7c167f45
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-11-15 12:15:47 -0800

    function to check if they have moderator role to allow access, otherwise send message

--- middleware/authJwt.js
+++ middleware/authJwt.js
@@ -40,3 +40,20 @@ isAdmin = (req, res, next) => {
         });
     });
 };
+
+isModerator = (req, res, next) => {
+    User.findByPk(req.userId).then(user => {
+        user.getRoles().then(roles => {
+            for (let i = 0; i < roles.length; i++) {
+                if (roles[i].name === 'moderator') {
+                    next();
+                    return;
+                }
+            };
+
+            res.status(403).send({
+                message: 'Require Moderator Role!'
+            });
+        });
+    });
+};
