9364b98b2dbeffd52d5f676bbf19000a61e07309
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-11-15 13:30:28 -0800

    function checks that role is a present option from ROLES otherwise sends message

--- middleware/verifySignup.js
+++ middleware/verifySignup.js
@@ -31,3 +31,18 @@ checkDuplicateUsernameOrEmail = (req, res, next) => {
         });
     });
 };
+
+checkRolesExisted = (req, res, next) => {
+    if (req.body.roles) {
+        for (let i = 0; i < roles.length; i++) {
+            if (!ROLES.includes(req.body.roles[i])) {
+                res.status(400).send({
+                    message: 'Failed! Role does not exist = ' + role.req.roles[i]
+                });
+                return;
+            }
+        }
+    }
+
+    next();
+};
